1d1e0f3be002e149b6bc5a0653edbeb61cd70d5b
Raayane Custodio <raayanecustodio@gmail.com~>
2024-07-05 11:11:29 -0300

    Adicionada tentativas ao codigo

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,6 +1,7 @@
 alert("Boas Vindas ao jogo do numero secreto");
 let numeroSecreto = 19;
 let chute;
+let tentativas =1;
 
 
 // Enquanto chute nao for igual ao n.s.
@@ -8,7 +9,7 @@ while (chute != numeroSecreto) {
     chute = prompt("Escolha um número entre 1 e 30")
 
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o numero secreto ${numeroSecreto} `);
+        alert(`Isso ai! Você descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
         console.log(numeroSecreto)
     } else {
         if (chute > numeroSecreto) {
@@ -16,6 +17,8 @@ while (chute != numeroSecreto) {
         } else {
             alert(`O numero secreto é maior que ${chute}`);
         }
+        // tentativas = tentativas + 1;
+        tentativas++;
 
     }
 }
