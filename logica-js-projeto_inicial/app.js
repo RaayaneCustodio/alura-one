101281d23c3ca7f72c0e1679c6129febca295f1e
Raayane Custodio <raayanecustodio@gmail.com~>
2024-07-05 11:04:31 -0300

    Adicionada a condição while

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,22 @@
 alert("Boas Vindas ao jogo do numero secreto");
-let numeroSecreto= 19;
-let chute = prompt("Escolha um número entre 1 e 30");
+let numeroSecreto = 19;
+let chute;
 
-if (chute == numeroSecreto){
-    alert(`Isso ai! Você descobriu o numero secreto ${numeroSecreto} ` );
-    console.log(numeroSecreto)
-} else {
-    alert("Você errou ! :(")
+
+// Enquanto chute nao for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt("Escolha um número entre 1 e 30")
+
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o numero secreto ${numeroSecreto} `);
+        console.log(numeroSecreto)
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O numero secreto é menor que ${chute}`);
+        } else {
+            alert(`O numero secreto é maior que ${chute}`);
+        }
+
+    }
 }
+//se chute for igual ao numero secreto
