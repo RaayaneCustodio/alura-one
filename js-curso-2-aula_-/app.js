20e3122c98d7e4ef76af8e4e789ada5c0d7b2671
Rayane <raayanecustodio@gmail.com>
2024-07-16 16:27:35 -0300

    alterações no projeto

---
+++ js-curso-2-aula_-/app.js
@@ -0,0 +1,41 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag)
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto')
+exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    console.log(numeroSecreto);
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Você acertou');
+        let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa' ;
+        let mensagemTentativas = `Você descobriu o numero secreto com ${tentativas} ${palavraTentativas}`; 
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O numero secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O numero secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
